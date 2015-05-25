1fe1ddd4def99e1103e58d42e75cd1fb6f747562
Majid Golshadi <golshadi.majid@gmail.com>
2015-05-25 17:57:14 +0430

    config file change

--- farmer.conf.example.js
+++ farmer.conf.example.js
@@ -1,14 +1,23 @@
 module.exports = {
     PORT: '8080',
-    CONTAINER_SERVER_API: 'http://xxxxxxx:4242',
-    RABBITMQ_CONFIG: {
-        host: 'amqp://localhost:5657',
-        login: 'xxxx',
-        password: 'xxxxx'
-    },
     LOG_DIR: '/xx/xxx/xxx',
     STORAGE: '/xxx',
     DOMAIN: 'xxxx.xx',
+    DB_USERNAME: 'xxxx',
+    DB_PASSWORD: 'xxxxx',
+    DB_NAME: 'farmer',
+    DB_HOST: 'xxx.xxx.xxx.xxx',
+    DB_DIALECT: 'mysql',
+    DB_PORT: '3306',
+    CONTAINER: {
+        types: ['docker'],
+        default: 'docker'
+    },
+    RABBITMQ_CONFIG: {
+        host: 'amqp://xxx.xxx.xxx.xxx:5657',
+        login: 'xxxx',
+        password: 'xxxxx'
+    },
     SSH_CONFIG: {
         privateKey: '/PRIVATE/KEY/FILE/ADDRESS',
         username: 'xxxx',
@@ -18,12 +27,12 @@ module.exports = {
     REVERSE_PROXY: {
         rootConfig: 'REVERSE PROXY conf.d FOLDER ADDRESS',
         containerID: 'REVERSE_PROXY_CONTAINER_ID',
-        destination_server_ip: 'xxx.xxx.xxx.xxx'
+        server: 'SERVER_TAG'
     },
-    DB_USERNAME: 'xxxx',
-    DB_PASSWORD: 'xxxxx',
-    DB_NAME: 'farmer',
-    DB_HOST: 'xxx.xxx.xxx.xxx',
-    DB_DIALECT: 'mysql',
-    DB_PORT: '3306'
+    SERVERS: {
+        SERVER_TAG: {
+            docker_api: 'http://xxxx:xxx',
+            ip: 'xxx.xxx.xxx.xxx'
+        }
+    }
 };
