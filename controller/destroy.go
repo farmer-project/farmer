0583fa9b47f11398d3c65dd0822e77d28985e94e
Majid Golshadi <golshadi.majid@gmail.com>
2015-08-29 11:58:48 +0430

    bugfix(destroy): Delete a box domains when destroying it

--- controller/destroy.go
+++ controller/destroy.go
@@ -3,12 +3,17 @@ package controller
 import (
 	"github.com/farmer-project/farmer/db"
 	"github.com/farmer-project/farmer/farmer"
+	"github.com/farmer-project/farmer/reverse_proxy"
 )
 
 func BoxDestroy(name string) error {
 	box, err := farmer.FindBoxByName(name)
 
 	if err != nil {
+		return err
+	}
+
+	if err := reverse_proxy.DeleteDomains(box); err != nil {
 		return err
 	}
 
