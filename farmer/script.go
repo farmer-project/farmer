c3313761c0e7836d4e73fabf6b4ae17f44f15699
aramalipoor <aram.alipoor@gmail.com>
2015-08-31 10:54:09 +0430

    bugfix(script): Remove sh before running scripts

--- farmer/script.go
+++ farmer/script.go
@@ -9,7 +9,6 @@ const (
 func (b *Box) runScript(key string) error {
 	if _, ok := b.Scripts[key]; ok {
 		return dockerExecOnContainer(b, []string{
-			"sh",
 			b.Home + "/" + b.Scripts[key],
 		})
 	}
